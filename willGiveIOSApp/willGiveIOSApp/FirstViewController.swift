0b925b15e042cd0c10de391bd6fd9879f7cde4db
Cyrus Zhang <cyrus.ylzhang@gmail.com>
2015-01-24 11:59:25 -0800

    Initial Commit

---
+++ willGiveIOSApp/willGiveIOSApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  willGiveIOSApp
+//
+//  Created by Cyrus Zhang on 1/24/15.
+//  Copyright (c) 2015 Cyrus Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ willGiveIOSApp/willGiveIOSApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  willGiveIOSApp
+//
+//  Created by Cyrus Zhang on 1/24/15.
+//  Copyright (c) 2015 Cyrus Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
