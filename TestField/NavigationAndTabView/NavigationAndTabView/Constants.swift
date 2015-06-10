6259f017d8d9b8c565634e1c42c3a464e9be7ead
Cyrus Zhang <cyrus.ylzhang@gmail.com>
2015-06-09 22:23:45 -0700

    add icons and launch screens

--- TestField/NavigationAndTabView/NavigationAndTabView/Constants.swift
+++ TestField/NavigationAndTabView/NavigationAndTabView/Constants.swift
@@ -12,7 +12,7 @@ import UIKit
 let BaseGreen = UIColor(red :120/255.0, green : 220/255.0, blue : 150/255.0, alpha : 0.8)
 let BaseGray = UIColor(red: 210/255.0, green : 210/255.0, blue : 210/255.0, alpha : 1.0)
 
-let ServerAddress : String = "54.174.120.77"
+let ServerAddress : String = "willgive.org"
 let SignInURL : String = "https://" + ServerAddress + "/services/login/mobileSignin"
 let SignUpURL : String = "https://" + ServerAddress + "/services/login/mobileSignup"
 let ListCharityURL : String = "https://" + ServerAddress + "/services/charities/listAllCharity"
